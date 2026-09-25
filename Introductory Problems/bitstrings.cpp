6f4e61401a652b239439e8795a961a3a0dd31626
ypa-me <224218152+ypa-me@users.noreply.github.com>
2026-09-25 05:41:15 +0400

    Solved: Bit strings

---
+++ Introductory Problems/bitstrings.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+#define ll long long
+using namespace std;
+// this is very simple
+// 2^n is the number of bits a n spaces can form
+
+
+
+
+ll MOD = 1e9 + 7;
+
+// Fast Exponentiation
+ll power(ll base, ll expo) {
+    ll ans = 1;
+    while(expo) {
+        if(expo & 1LL) {
+            ans = (ans * base) % MOD;
+        }
+        base = (base * base) % MOD;
+        expo >>= 1LL;
+    }
+    return ans;
+}
+
+
+
+int main() {
+  ios_base::sync_with_stdio(false);
+cin.tie(nullptr);
+
+  ll n;
+  cin >>n;
+ cout << power(2LL, n) << endl;
+ return 0;
+    
+}
+
